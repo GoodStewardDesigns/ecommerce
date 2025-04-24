5c9a775671ecd266bffba0c5e6a6b59b9fe8573c
GSD-Desktop <brandon@goodstewarddesigns.com>
2025-04-24 10:45:04 -0400

    created new functions and files specific for ecom.

---
+++ includes/ecommerce.config.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+    *   File name: ecommerce.config.inc.php
+    *   Created by: Brandon Hills
+    *   Contact: brandon@goodstewarddesigns.com
+    *   Last modified: 4/24/25
+    *
+    *   - Define ecommerce/billing config here:
+*/
---
+++ includes/ecommerce.functions.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+    *   File name: functions.inc.php
+    *   Created by: Brandon Hills
+    *   Contact: brandon@goodstewarddesigns.com
+    *   Last modified: 4/24/25
+    *
+    *   - Define reusable ecommerce specific functions here:
+*/
+
+# ***************** #
+# **** ROUTING **** #
+
+function get_product_by_slug($slug) {
+    //  Placeholder logic - replace with DB call
+
+    return [
+        'name' => 'Sample Product',
+        'slug' => $slug,
+        'price' => 19.99
+    ];
+}
+
+# **** ROUTING **** #
+# ***************** #
